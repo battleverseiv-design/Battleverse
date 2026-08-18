746a08ffc957836e230cd49c1ae9e4f0b3a7a5ac
battleverseiv-design <battleverseiv@gmail.com>
2026-08-18 18:50:34 +0200

    Update client build v1.1.74 (100 files, 507.5 MB)

--- client_files/kubejs/startup_scripts/item/bombardment.js
+++
@@ -1,9 +0,0 @@
-// startup_scripts/custom_items.js
-
-StartupEvents.registry('item', event => {
-    event.create('laser_designator')
-        .displayName('§bОгневая поддержка [Град]')
-        .unstackable()
-        .tooltip('§7Вызывает мгновенный кассетный удар по точке наведения.')
-        .tooltip('§eОперативное время подлета: 5 сек.')
-})
